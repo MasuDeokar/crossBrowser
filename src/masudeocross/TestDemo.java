64f7fc3a577504999aca1a7d8ba61b73de62e66f
MasuDeokar <mayureshdeokar@gmail.com>
2024-02-08 00:00:13 +0530

    testNg .xml files updated

--- src/masudeocross/TestDemo.java
+++ src/masudeocross/TestDemo.java
@@ -11,19 +11,15 @@ import org.testng.annotations.Test;
 
 public class TestDemo {
 
-	
 	@SuppressWarnings("deprecation")
-	@Test 
-	public void titleCheck() throws MalformedURLException
-	{
+	@Test
+	public void titleCheck() throws MalformedURLException {
 		MutableCapabilities caps = new MutableCapabilities();
-		//WebDriver driver = new RemoteWebDriver(new URL("http://hub.browserstack.com/wd/hub"), caps);
-		WebDriver driver = new RemoteWebDriver(new URL("https://mayureshdeokar_5Geiga:8PhvyppcqpyMH5DzpFpr@hub-cloud.browserstack.com/wd/hub"), caps);
+		// WebDriver driver = new RemoteWebDriver(new
+		// URL("http://hub.browserstack.com/wd/hub"), caps);
+		WebDriver driver = new RemoteWebDriver(
+				new URL("https://mayureshdeokar_5Geiga:8PhvyppcqpyMH5DzpFpr@hub-cloud.browserstack.com/wd/hub"), caps);
 		driver.get("https://www.google.com/");
 		Assert.assertTrue(driver.getTitle().matches("Google"));
-		
 	}
-
-	}
-
-
+}
